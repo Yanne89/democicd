d58523c5e1822b1c2d7771fa9b5d333a90372c20
Yanne <yannemanuela@gmail.com>
2025-04-02 00:00:14 -0400

    update made to workflow

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-effulgence"            # Replace with your S3 bucket name
+    bucket         = "yannesource"            # Replace with your S3 bucket name
     key            = "webapp/terraform.tfstate" # Path to the state file in the bucket
     region         = "us-east-1"                              # Replace with your AWS region
     # dynamodb_table = "terraform-lock-table"                   # Replace with your DynamoDB table name
