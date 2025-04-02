01120bae079739e688d8531fdacd5f45de49628d
Yanne <yannemanuela@gmail.com>
2025-04-01 21:31:22 -0400

    new folder created and files moved

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 variable "vpc_name" {
   description = "The name of the VPC"
   type        = string
-  default     = "main-vpc"
+  default     = "ASG-vpc"
 }
 
 variable "ami_id" {
