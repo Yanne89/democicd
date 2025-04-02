40562b16c2c42596beeb64d042dfa31911316486
Yanne <yannemanuela@gmail.com>
2025-04-01 22:20:32 -0400

    made a change to the variable file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-terraform-state-effulgence"            # Replace with your S3 bucket name
     key            = "aws-autoscale-webapp/terraform.tfstate" # Path to the state file in the bucket
     region         = "us-east-1"                              # Replace with your AWS region
-    dynamodb_table = "terraform-lock-table"                   # Replace with your DynamoDB table name
+    # dynamodb_table = "terraform-lock-table"                   # Replace with your DynamoDB table name
     encrypt        = true                                     # Encrypt the state file
   }
 }
