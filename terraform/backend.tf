327617ec8ad16a5992fc3fc659ebdfe18bd8b8bc
Yanne <yannemanuela@gmail.com>
2025-04-01 21:57:34 -0400

    new folder created and files moved

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"            # Replace with your S3 bucket name
+    bucket         = "my-bans-terraform-state-bucket"            # Replace with your S3 bucket name
     key            = "aws-autoscale-webapp/terraform.tfstate" # Path to the state file in the bucket
     region         = "us-east-1"                              # Replace with your AWS region
     dynamodb_table = "terraform-lock-table"                   # Replace with your DynamoDB table name
